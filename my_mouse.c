d18657809ab31d0ad9d6d4ac2837879b22c75853
alan chang <chang_a@qwasar.io>
2023-10-05 20:59:13 +0000

    reading map file

--- my_mouse.c
+++ my_mouse.c
@@ -1,14 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
 
 
 
+int main(int av, char** ac){
 
-#define FULL '#'
-#define EMPTY ' '
-#define PATH 'o'
-#define ENTRANCE '1'
-#define EXIT '2'
+//  1. read map while checking if map is valid
 
-//  1. read map and check if map is valid
-//  2. if map is valid, construct corresponding matrix/graph
-//  3. find shortest path
+    if (av != 2) {
+        write(2, "invalid number of arguments\n", 29); 
+        return 1;
+    }
+
+    FILE* map_file = fopen(ac[1], "r");
+    if (map_file == NULL) {
+        write(2, "invalid file\n", 13);
+        return 1;
+    }
+
+    char line[1000];
+
+    while (fgets(line, sizeof(line), map_file) != NULL) {
+        printf("%s", line);
+    }
+
+    fclose(map_file);
+
+//  2. Construct corresponding matrix/graph
+
+//  3. Find shortest path
+
 //  4. reconstruct/print map to stdout with "path" characters indicating shortest path
+
+    return 0;
+}
