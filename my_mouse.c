12fd962ac453e1fcc671fbf1457696223bd400fb
alan chang <chang_a@qwasar.io>
2023-10-05 18:04:17 +0000

    added more boilerplate and thought process

---
+++ my_mouse.c
@@ -0,0 +1,14 @@
+
+
+
+
+#define FULL '#'
+#define EMPTY ' '
+#define PATH 'o'
+#define ENTRANCE '1'
+#define EXIT '2'
+
+//  1. read map and check if map is valid
+//  2. if map is valid, construct corresponding matrix/graph
+//  3. find shortest path
+//  4. reconstruct/print map to stdout with "path" characters indicating shortest path
