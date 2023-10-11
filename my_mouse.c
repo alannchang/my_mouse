c301c053b44a2a9ee046593d699bec7a6b4cfc72
alan chang <chang_a@qwasar.io>
2023-10-11 18:52:12 +0000

    baby steps

--- my_mouse.c
+++ my_mouse.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdbool.h>
+
+typedef struct Node {
+    int row, col, f, g, h;
+} cell;
 
 int get_dimensions(FILE* map_file, char* width, char* trailing_char){
 
@@ -25,7 +31,7 @@ int get_dimensions(FILE* map_file, char* width, char* trailing_char){
     return 0;
 }
 
-int create_maze(FILE* map_file, int total_rows, int total_columns, int (*maze)[total_columns + 1], char full, char empty, char maze_entrance, char maze_exit){
+int create_maze(FILE* map_file, int total_columns, int (*maze)[total_columns + 1], char full, char empty, char maze_entrance, char maze_exit, cell* entry_cell, cell* exit_cell){
 
     char temp;
     int row_index = 0;
@@ -44,11 +50,15 @@ int create_maze(FILE* map_file, int total_rows, int total_columns, int (*maze)[t
         }
 
         else if (temp == maze_entrance) {
+            entry_cell->row = row_index;
+            entry_cell->col = col_index;
             maze[row_index][col_index] = 2;
             col_index++;
         } 
         
         else if (temp == maze_exit) {
+            exit_cell->row = row_index;
+            exit_cell->col = col_index;
             maze[row_index][col_index] = 3;
             col_index++;
         } 
@@ -65,11 +75,82 @@ int create_maze(FILE* map_file, int total_rows, int total_columns, int (*maze)[t
     return 0;
 }
 
+bool is_valid(int row, int col, int total_rows, int total_columns){
+    return (row >= 0) && (row < total_rows) && (col >= 0) && (col < total_columns);
+}
 
+bool is_not_blocked(int total_columns, int (*maze)[total_columns + 1],  int row, int col){
+    return (maze[row][col] == 1);
+}
+
+bool is_exit(int row, int col, cell exit_cell){
+    return (row == exit_cell.row) && (col == exit_cell.col);
+}
+
+int get_h(int row, int col, cell exit_cell){
+    int h = abs(row - exit_cell.row) + abs(col - exit_cell.col); 
+    return h;
+}
+
+void trace_path(cell exit_cell){
+
+}
+
+int a_star(int total_columns, int (*maze)[total_columns + 1]){
+    
+    return 0;
+}
+/*
+
+// A* Search Algorithm
+1.  Initialize the open list
+2.  Initialize the closed list
+    put the starting node on the open 
+    list (you can leave its f at zero)
+
+3.  while the open list is not empty
+    a) find the node with the least f on 
+       the open list, call it "q"
+
+    b) pop q off the open list
+  
+    c) generate q's 8 successors and set their 
+       parents to q
+   
+    d) for each successor
+        i) if successor is the goal, stop search
+        
+        ii) else, compute both g and h for successor
+          successor.g = q.g + distance between 
+                              successor and q
+          successor.h = distance from goal to 
+          successor (This can be done using many 
+          ways, we will discuss three heuristics- 
+          Manhattan, Diagonal and Euclidean 
+          Heuristics)
+          
+          successor.f = successor.g + successor.h
+
+        iii) if a node with the same position as 
+            successor is in the OPEN list which has a 
+           lower f than successor, skip this successor
+
+        iV) if a node with the same position as 
+            successor  is in the CLOSED list which has
+            a lower f than successor, skip this successor
+            otherwise, add  the node to the open list
+     end (for loop)
+  
+    e) push q on the closed list
+    end (while loop)
+
+*/
 
 int main(int av, char** ac){
 
 //  1. read map while checking if map is valid
+
+//  FIRST LINE: PARAMETERS
 
     if (av != 2) {
         write(2, "invalid number of arguments\n", 29); 
@@ -112,12 +193,16 @@ int main(int av, char** ac){
         return 1;
     }
 
+//  MAP
+
     int total_rows = atoi(width);
     int total_columns = atoi(length);
 
     int maze[total_rows + 1][total_columns + 1];
+    cell entry_cell;
+    cell exit_cell;
 
-    if (create_maze(map_file, total_rows, total_columns, maze, full, empty, maze_entrance, maze_exit) != 0){
+    if (create_maze(map_file, total_columns, maze, full, empty, maze_entrance, maze_exit, &entry_cell, &exit_cell) != 0){
         write(2, "MAP ERROR", 10);
         return 1;
     }
@@ -129,13 +214,13 @@ int main(int av, char** ac){
         printf("\n");
     }
 
+    printf("Entrance: %d, %d\nExit: %d, %d\n", entry_cell.row, entry_cell.col, exit_cell.row, exit_cell.col);
+
+
     fclose(map_file);
 
 //  2. Construct corresponding matrix/graph
-
 //  3. Find shortest path
-
 //  4. reconstruct/print map to stdout with "path" characters indicating shortest path
-
     return 0;
 }
