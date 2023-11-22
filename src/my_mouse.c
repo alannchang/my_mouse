7b1c36c61d176aff16c9ceeb78c64da1d3bb7c5d
alan chang <chang_a@qwasar.io>
2023-11-22 00:18:54 +0000

    two leaks to go

--- src/my_mouse.c
+++ src/my_mouse.c
@@ -13,8 +13,8 @@ int main(int ac, char** av){
 
     parameter param = set_parameters(map_file);
     symbol symbol = set_symbols(param.trailing_char, param.ptr);
-    free(param.first_line);
     map map = init_map(param.width, param.length);
+    free(param.first_line);
     free(param.width);
     free(param.length);
     cell entry_cell, exit_cell;
@@ -22,8 +22,9 @@ int main(int ac, char** av){
     if ((path_list = solve_map(map_file, map, symbol, &entry_cell, &exit_cell, &path_list)) == NULL){
         write(2, "MAP ERROR", 9);
         return 1;     
-    }   
+    }
     printer(map, symbol, path_list);
+    for (int i = 0; i < map.total_rows + 1; i++) free(map.arr[i]);
     free(map.arr);
     return 0;
 }
